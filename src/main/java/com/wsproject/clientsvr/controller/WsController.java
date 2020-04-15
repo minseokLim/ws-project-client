f8111a110ef46e2c011543e3a70f265ed71cb131
mslim <mslim8803@naver.com>
2020-04-15 20:02:15 +0900

    명언 관련 RestController 생성
    rest 관련 함수 별도의 RestController로 뺌

--- src/main/java/com/wsproject/clientsvr/controller/WsController.java
+++ src/main/java/com/wsproject/clientsvr/controller/WsController.java
@@ -1,20 +1,15 @@
 package com.wsproject.clientsvr.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 import com.wsproject.clientsvr.dto.TodaysWs;
 import com.wsproject.clientsvr.dto.TokenInfo;
 import com.wsproject.clientsvr.dto.UserInfo;
-import com.wsproject.clientsvr.dto.WsPsl;
 import com.wsproject.clientsvr.util.CommonUtil;
 import com.wsproject.clientsvr.util.RestUtil;
 
@@ -54,23 +49,5 @@ public class WsController {
 	@GetMapping("/ws-service/saveWsPage")
 	public String saveWsPage() {
 		return "ws-service/saveWsPage";
-	}
-	
-	@ResponseBody
-	@PostMapping("/ws-service/wsPsls")
-	public ResponseEntity<String> insertWsPersonal(@CookieValue("userInfo") String userCookie, @CookieValue("tokenInfo") String tokenCookie, @RequestBody WsPsl wsPsl) {
-		
-		UserInfo userInfo = commonUtil.extractCookie(userCookie, UserInfo.class);
-		TokenInfo tokenInfo = commonUtil.extractCookie(tokenCookie, TokenInfo.class);
-		
-		wsPsl.setOwnerIdx(userInfo.getIdx());
-		
-		RestUtil restUtil = RestUtil.builder().url("/ws-service/v1.0/users/" + userInfo.getIdx() + "/wses")
-											  .post().tokenInfo(tokenInfo).bodyParam(wsPsl)
-											  .build();
-	
-		ResponseEntity<String> entity = restUtil.exchange();
-		
-		return new ResponseEntity<String>(entity.getBody(), HttpStatus.CREATED);
 	}
 }
---
+++ src/main/java/com/wsproject/clientsvr/controller/WsRestController.java
@@ -0,0 +1,40 @@
+package com.wsproject.clientsvr.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wsproject.clientsvr.dto.TokenInfo;
+import com.wsproject.clientsvr.dto.UserInfo;
+import com.wsproject.clientsvr.dto.WsPsl;
+import com.wsproject.clientsvr.util.CommonUtil;
+import com.wsproject.clientsvr.util.RestUtil;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class WsRestController {
+	
+	private CommonUtil commonUtil;
+	
+	@PostMapping("/ws-service/wsPsls")
+	public ResponseEntity<String> insertWsPersonal(@CookieValue("userInfo") String userCookie, @CookieValue("tokenInfo") String tokenCookie, @RequestBody WsPsl wsPsl) {
+		
+		UserInfo userInfo = commonUtil.extractCookie(userCookie, UserInfo.class);
+		TokenInfo tokenInfo = commonUtil.extractCookie(tokenCookie, TokenInfo.class);
+		
+		wsPsl.setOwnerIdx(userInfo.getIdx());
+		
+		RestUtil restUtil = RestUtil.builder().url("/ws-service/v1.0/users/" + userInfo.getIdx() + "/wses")
+											  .post().tokenInfo(tokenInfo).bodyParam(wsPsl)
+											  .build();
+	
+		ResponseEntity<String> entity = restUtil.exchange();
+		
+		return new ResponseEntity<String>(entity.getBody(), HttpStatus.CREATED);
+	}
+}

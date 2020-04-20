c3222388b42d35926e1a2820a70c088b78d9a3b7
mslim <mslim8803@naver.com>
2020-04-21 01:04:08 +0900

    으어어어 alert을 통한 에러로그 추가

--- src/main/resources/static/js/Common.js
+++ src/main/resources/static/js/Common.js
@@ -26,31 +26,10 @@ var commonJS = {
                 apiUrl: apiUrl
             },
             success: function(result) {
-            	if(result._embedded) {
-            		var wsPslList = result._embedded.wsPslDtoList;
-                    var rows = '';
-                    
-                    for(var i = 0; i < wsPslList.length; i++) {
-                    	var content = wsPslList[i].content;
-                    	var author = wsPslList[i].author;
-                    	
-                    	if(content.length > 11) content = content.substring(0, 10) + '...';
-                    	if(author.length > 7) author = author.substring(0, 6) + '...';
-                    	
-                        rows += '<tr data-href="' + wsPslList[i]._links.self.href + '" onclick="viewWsPsl(this);">' +
-                                    '<td>' + content + '</td>' +
-                                    '<td>' + author + '</td>' +
-                                '</tr>';
-                    }
-                    
-                    $('#tableBody').append(rows);
-                    
-                    if(result._links.next) {
-                        nextUri = result._links.next.href;
-                    } else {
-                        nextUri = null;
-                    }
-            	}
+            	callbackFunc(result);
+            },
+            error:function(request, status, error){
+            	alert("code : " + request.status + "\n" + "message : " + request.responseText + "\n" + "error : " + error);
             }
 		});
 	},
@@ -65,8 +44,10 @@ var commonJS = {
             data: JSON.stringify(data),
             contentType : "application/json; charset=UTF-8",
             success: function(result) {
-            	alert('저장되었습니다.');
-                window.location.href = '/ws-service/wsPslList';
+            	callbackFunc(result);
+            },
+            error:function(request, status, error){
+            	alert("code : " + request.status + "\n" + "message : " + request.responseText + "\n" + "error : " + error);
             }
         });
 	}
